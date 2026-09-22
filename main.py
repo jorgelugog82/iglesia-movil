0527ddb70ea01a1c89de01a3fcdc6a888a2aaa23
jorgelugog82 <102298968+jorgelugog82@users.noreply.github.com>
2026-09-22 01:05:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-import flet as ft
+import webbrowser
 import time
+import sys
 
-def main(page: ft.Page):
-    page.title = "Conector Iglesia"
-    
+def main():
     # Dirección IP local del Servidor Central de tu iglesia (tu PC)
     IP_SERVIDOR = "192.168.0.111" 
     PUERTO = "8550"
     url_servidor = f"http://{IP_SERVIDOR}:{PUERTO}"
     
-    # Ordena a Android abrir de forma inmediata Google Chrome o el navegador nativo
-    page.launch_url(url_servidor)
-    
-    # Le damos 3 segundos al teléfono para procesar la apertura de la página
-    time.sleep(3)
-    page.window_close()
-# Cierre del archivo usando el formato obligatorio del empaquetador de Android
+    # Forzamos a Android a abrir el panel de salones directamente en Google Chrome/Safari
+    webbrowser.open(url_servidor)
+
 if __name__ == "__main__":
-    ft.app(target=main)
+    # Pausa de un segundo para asegurar que los servicios de red del celular respondan
+    time.sleep(1)
+    main()
+    # Forzamos el cierre del conector invisible para no gastar batería en el celular
+    sys.exit(0)
